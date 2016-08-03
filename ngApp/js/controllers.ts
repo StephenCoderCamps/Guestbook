57799b32e7dc3cc2177151ef836c85f5e6f345c8
RegB <beason.reginald@gmail.com>
2016-08-03 14:47:42 -0700

    1st Controller/Service

--- ngApp/js/controllers.ts
+++ ngApp/js/controllers.ts
@@ -1,7 +1,13 @@
 namespace app.Controllers {
   export class HomeController {
+      public guestbook;
+      public save() {
+        this.messageService.save(this.guestbook).then(() =>{
+          this.$state.go(`Home`)
+        })
+      }
 
-    constructor() {
+        constructor(private messageService:app.Services.MessageService, private $state:ng.ui.IStateService) {
 
     }
   }
--- ngApp/js/services.ts
+++ ngApp/js/services.ts
@@ -1,3 +1,16 @@
 namespace app.Services {
 
+      export class MessageService {
+        private MessageResource;
+
+        public save(guestbook) {
+          return this.MessageResource.save(guestbook).$promise;
+        }
+
+        constructor(private $resource:ng.resource.IResourceService) {
+          this.MessageResource = $resource(`/api/guestbook/:id`);
+        }
+      }
+
+      angular.module('app').service('messageService', MessageService);
 }
